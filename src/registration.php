0845bb440459fd9ec310ea0e0b8f4b973a777722
Josh Johnson <josh@grafikchaos.com>
2023-01-31 19:12:01 -0600

    Fix code quality issue

--- src/registration.php
+++ src/registration.php
@@ -1,6 +1,9 @@
 <?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
 \Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    ComponentRegistrar::MODULE,
     'Sebwite_ProductDownloads',
     __DIR__
 );
